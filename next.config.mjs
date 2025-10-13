b08448618c28a68045640e489e94014b878afbd6
Alvin Varghese <alvin19official@gmail.com>
2025-10-14 00:31:14 +0530

    fix: img alias config

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "www.ritu25.live",
+        hostname: process.env.NEXT_PUBLIC_LOGO_DOMAIN,
         port: "",
         pathname: "**",
       },
